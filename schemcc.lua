1a9468d2916d6cc37c17f323eb0f0bcb2a37ed9a
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2021-03-26 11:26:24 +0100

    basic schemcc

---
+++ schemcc.lua
@@ -0,0 +1,331 @@
+local args = {...}
+local building
+local length
+local height
+local width
+local blocks
+
+-- JSON library made by ElvishJerricco
+-- http://www.computercraft.info/forums2/index.php?/topic/5854-json-api-v201-for-computercraft/
+
+------------------------------------------------------------------ utils
+local controls = {["\n"]="\\n", ["\r"]="\\r", ["\t"]="\\t", ["\b"]="\\b", ["\f"]="\\f", ["\""]="\\\"", ["\\"]="\\\\"}
+ 
+local function isArray(t)
+    local max = 0
+    for k,v in pairs(t) do
+        if type(k) ~= "number" then
+            return false
+        elseif k > max then
+            max = k
+        end
+    end
+    return max == #t
+end
+ 
+local whites = {['\n']=true; ['\r']=true; ['\t']=true; [' ']=true; [',']=true; [':']=true}
+function removeWhite(str)
+    while whites[str:sub(1, 1)] do
+        str = str:sub(2)
+    end
+    return str
+end
+ 
+------------------------------------------------------------------ encoding
+ 
+local function encodeCommon(val, pretty, tabLevel, tTracking)
+    local str = ""
+ 
+    -- Tabbing util
+    local function tab(s)
+        str = str .. ("\t"):rep(tabLevel) .. s
+    end
+ 
+    local function arrEncoding(val, bracket, closeBracket, iterator, loopFunc)
+        str = str .. bracket
+        if pretty then
+            str = str .. "\n"
+            tabLevel = tabLevel + 1
+        end
+        for k,v in iterator(val) do
+            tab("")
+            loopFunc(k,v)
+            str = str .. ","
+            if pretty then str = str .. "\n" end
+        end
+        if pretty then
+            tabLevel = tabLevel - 1
+        end
+        if str:sub(-2) == ",\n" then
+            str = str:sub(1, -3) .. "\n"
+        elseif str:sub(-1) == "," then
+            str = str:sub(1, -2)
+        end
+        tab(closeBracket)
+    end
+ 
+    -- Table encoding
+    if type(val) == "table" then
+        assert(not tTracking[val], "Cannot encode a table holding itself recursively")
+        tTracking[val] = true
+        if isArray(val) then
+            arrEncoding(val, "[", "]", ipairs, function(k,v)
+                str = str .. encodeCommon(v, pretty, tabLevel, tTracking)
+            end)
+        else
+            arrEncoding(val, "{", "}", pairs, function(k,v)
+                assert(type(k) == "string", "JSON object keys must be strings", 2)
+                str = str .. encodeCommon(k, pretty, tabLevel, tTracking)
+                str = str .. (pretty and ": " or ":") .. encodeCommon(v, pretty, tabLevel, tTracking)
+            end)
+        end
+    -- String encoding
+    elseif type(val) == "string" then
+        str = '"' .. val:gsub("[%c\"\\]", controls) .. '"'
+    -- Number encoding
+    elseif type(val) == "number" or type(val) == "boolean" then
+        str = tostring(val)
+    else
+        error("JSON only supports arrays, objects, numbers, booleans, and strings", 2)
+    end
+    return str
+end
+ 
+function encode(val)
+    return encodeCommon(val, false, 0, {})
+end
+ 
+function encodePretty(val)
+    return encodeCommon(val, true, 0, {})
+end
+ 
+------------------------------------------------------------------ decoding
+ 
+local decodeControls = {}
+for k,v in pairs(controls) do
+    decodeControls[v] = k
+end
+ 
+function parseBoolean(str)
+    if str:sub(1, 4) == "true" then
+        return true, removeWhite(str:sub(5))
+    else
+        return false, removeWhite(str:sub(6))
+    end
+end
+ 
+function parseNull(str)
+    return nil, removeWhite(str:sub(5))
+end
+ 
+local numChars = {['e']=true; ['E']=true; ['+']=true; ['-']=true; ['.']=true}
+function parseNumber(str)
+    local i = 1
+    while numChars[str:sub(i, i)] or tonumber(str:sub(i, i)) do
+        i = i + 1
+    end
+    local val = tonumber(str:sub(1, i - 1))
+    str = removeWhite(str:sub(i))
+    return val, str
+end
+ 
+function parseString(str)
+    str = str:sub(2)
+    local s = ""
+    while str:sub(1,1) ~= "\"" do
+        local next = str:sub(1,1)
+        str = str:sub(2)
+        assert(next ~= "\n", "Unclosed string")
+ 
+        if next == "\\" then
+            local escape = str:sub(1,1)
+            str = str:sub(2)
+ 
+            next = assert(decodeControls[next..escape], "Invalid escape character")
+        end
+ 
+        s = s .. next
+    end
+    return s, removeWhite(str:sub(2))
+end
+ 
+function parseArray(str)
+    str = removeWhite(str:sub(2))
+ 
+    local val = {}
+    local i = 1
+    while str:sub(1, 1) ~= "]" do
+        local v = nil
+        v, str = parseValue(str)
+        val[i] = v
+        i = i + 1
+        str = removeWhite(str)
+    end
+    str = removeWhite(str:sub(2))
+    return val, str
+end
+ 
+function parseObject(str)
+    str = removeWhite(str:sub(2))
+ 
+    local val = {}
+    while str:sub(1, 1) ~= "}" do
+        local k, v = nil, nil
+        k, v, str = parseMember(str)
+        val[k] = v
+        str = removeWhite(str)
+    end
+    str = removeWhite(str:sub(2))
+    return val, str
+end
+ 
+function parseMember(str)
+    local k = nil
+    k, str = parseValue(str)
+    local val = nil
+    val, str = parseValue(str)
+    return k, val, str
+end
+ 
+function parseValue(str)
+    local fchar = str:sub(1, 1)
+    if fchar == "{" then
+        return parseObject(str)
+    elseif fchar == "[" then
+        return parseArray(str)
+    elseif tonumber(fchar) ~= nil or numChars[fchar] then
+        return parseNumber(str)
+    elseif str:sub(1, 4) == "true" or str:sub(1, 5) == "false" then
+        return parseBoolean(str)
+    elseif fchar == "\"" then
+        return parseString(str)
+    elseif str:sub(1, 4) == "null" then
+        return parseNull(str)
+    end
+    return nil
+end
+ 
+function decode(str)
+    str = removeWhite(str)
+    t = parseValue(str)
+    return t
+end
+ 
+function decodeFromFile(path)
+    local file = assert(fs.open(path, "r"))
+    local decoded = decode(file.readAll())
+    file.close()
+    return decoded
+end
+
+-- schemcc functions
+
+function build(building)
+	-- create vars
+	length = building.length
+	height = building.height
+	width = building.width
+	blocks = building.blocks
+	
+	-- move turtle
+	for x = 0, length do
+		local shouldGoZ = shouldGoAxis(x, y, z, "z")
+		if shouldGoZ then
+			turtle.turnRight()
+			for z = 0, width do
+				local shouldGoY = shouldGoAxis(x, y, z, "y")
+				if shouldGoY then
+					for y = 0, height do
+						turtle.up()
+						placeBlock(blocks, x, y, z)
+					end
+					turtle.forward()
+					for y = 0, height do
+						turtle.down()
+					end
+				else
+					turtle.forward()
+				end
+			end
+			turtle.turnLeft()
+			turtle.turnLeft()
+			for z = 0, width do
+				turtle.forward()
+			end
+			turtle.turnRight()
+			turtle.forward()
+		else
+			turtle.forward()
+		end
+	end
+	print("Coming back")
+	for x = 0, length do
+		turtle.back()
+	end
+	print("Done")
+end
+
+function shouldGoAxis(x, y, z, axis)
+	-- calculate if there are blocks on a specific axis
+	-- if the turtle should even go there
+	if axis == "y" then
+		for i = 0, height do
+			for key, block in pairs(blocks) do
+				if block then
+					if block.x == x and block.y == i and block.z == z then
+						return true
+					end
+				end
+			end
+		end
+	end
+	if axis == "z" then
+		for i = 0, width do
+			if shouldGoAxis(x, y, i, "y") then
+				return true
+			end
+		end
+	end
+	print("Skipping axis " .. axis)
+	return false
+end
+
+function placeBlock(blocks, x, y, z)
+	--	try to place the block
+	for key, block in pairs(blocks) do
+		if block.x == x and block.y == y and block.z == z then
+			if block.material then
+				selectBlock(block.material)
+				turtle.placeDown()
+				print("Placed block " .. block.material)
+				return true
+			end
+		end
+	end
+	return false
+end
+
+function selectBlock(material)
+	for i = 1, 16 do
+		local item = turtle.getItemDetail(i)
+		if(item) then
+			if(item.name == material) then
+				turtle.select(i)
+				return
+			end
+		end
+	end
+	print("Needs more "..material)
+	sleep(10)
+	selectBlock(material)
+end
+
+-- start
+-- read file
+local file = fs.open(args[1], "r")
+local jsonString = file.readAll()
+file.close()
+-- parse json
+print("Parsing file")
+building = decode(jsonString)
+build(building)
